c9f09812492316d347077a89ca93c5935bc654a7
Michele-Favia <michelefavia80@gmail.com>
2024-11-25 12:09:17 +0100

    esercizio 1

---
+++ script.js
@@ -0,0 +1,13 @@
+//ESERCIZIO 1
+
+// Dato un array di oggetti rappresentanti degli sviluppatori:
+
+// - crea una funzione che calcoli e restituisca il numero di sviluppatori di Javascript provenienti dall’europa
+
+
+let list = [
+    { firstName: 'Marco', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Simone', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Matteo', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Ettore', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+  ];
