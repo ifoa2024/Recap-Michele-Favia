3e0f0234dfe49849f24e067a8d9f747d30879712
Michele-Favia <michelefavia80@gmail.com>
2024-11-25 12:30:38 +0100

    esercizio 1 completato

--- esercizi.md
+++ esercizi.md
@@ -28,7 +28,7 @@ Dato un array di oggetti:
 
 ### ES.
 ```
-    let list = [
+    let list =   [
       { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
       { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
       { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
--- script.js
+++ script.js
@@ -4,21 +4,27 @@
 
 // - crea una funzione che calcoli e restituisca il numero di sviluppatori di Javascript provenienti dall’europa
 
-function addSaluto(list) {
-    return list.map(dev => {
-      dev.saluto = `Ciao ${dev.firstName}, ${dev.language}?`;
-      return dev; 
-    });
-  }
+// function countDevelopers(list, continent, language) {
+//     return list.filter(dev => dev.continent === continent && dev.language === language).length;
+//   }
   
-  let list = [
-    { firstName: 'Marco', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
-    { firstName: 'Simone', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
-    { firstName: 'Matteo', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
-    { firstName: 'Ettore', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
-  ];
+//   let list = [
+//     { firstName: 'Marco', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//     { firstName: 'Simone', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//     { firstName: 'Matteo', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//     { firstName: 'Ettore', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+//   ];
   
-  let updatedList = addSaluto(list);
+//   // Esempio di utilizzo della funzione:
+//   let continent = 'Europe';
+//   let language = 'JavaScript';
   
-  console.log(updatedList);
-  
+//   let count = countDevelopers(list, continent, language);
+//   console.log(`Numero di sviluppatori ${language} provenienti da ${continent}:`, count);
+// -------------------------------------------------------------------------------------------
+// ESERCIZIO 2
+//  Dato un array di oggetti:
+
+// - crea una funzione che ritorni un array
+// - e aggiunga a ogni oggetto la proprietà ‘greeting’ con il seguente valore:
+//     - “Awe < firstName >, io lavoro con < language here >, ma effettivamente è meglio javascript”
