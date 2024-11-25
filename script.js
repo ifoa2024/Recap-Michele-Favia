45a47ac07608f9c115dd0ed5c28ee30425c3833f
Michele-Favia <michelefavia80@gmail.com>
2024-11-25 12:14:47 +0100

    modifiche

--- script.js
+++ script.js
@@ -4,10 +4,21 @@
 
 // - crea una funzione che calcoli e restituisca il numero di sviluppatori di Javascript provenienti dall’europa
 
-
-let list = [
+function addSaluto(list) {
+    return list.map(dev => {
+      dev.saluto = `Ciao ${dev.firstName}, cosa ti piace di più di${dev.language}?`;
+      return dev; 
+    });
+  }
+  
+  let list = [
     { firstName: 'Marco', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
     { firstName: 'Simone', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
     { firstName: 'Matteo', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
     { firstName: 'Ettore', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
   ];
+  
+  let updatedList = addSaluto(list);
+  
+  console.log(updatedList);
+  
